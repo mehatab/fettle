83925341be07ca486698937650622d91f2da7821
mehatab <shaikh.mehatab@gmail.com>
2022-08-15 22:32:06 +0530

    custom domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== "production";
-
 const nextConfig = {
-  assetPrefix: !debug ? 'https://mehatab.github.io/fettle/' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
