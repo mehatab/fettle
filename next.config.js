dea07dc97aacf34279289071aee0652cd9afaac6
mehatab <shaikh.mehatab@gmail.com>
2022-08-15 21:32:33 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://cdn.statically.io/gh/fettle/mehatab.github.io/gh-pages/' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
