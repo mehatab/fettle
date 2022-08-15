f0047ddcc3ebebdd80abd1132f1f707d5a8b063c
mehatab <shaikh.mehatab@gmail.com>
2022-08-15 22:41:08 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const production = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: production ? '/' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
