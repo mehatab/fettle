7fce1d2d3fe619666888c6f9ba20d3620202593c
mehatab <shaikh.mehatab@gmail.com>
2022-08-15 18:31:28 +0530

    config

---
+++ src/next.config.js
@@ -0,0 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/fettle/mehatab.github.io/gh-pages/' : '',
+}
