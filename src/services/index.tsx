e066da410809705f875826888a6558f07001cb17
mehatab <mahatab.shaikh@cogoport.com>
2022-11-13 12:56:42 +0530

    default icon

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -26,7 +26,10 @@ const ServicesSection: NextPage = () => {
                         <path d="M256 48C141.31 48 48 141.31 48 256s93.31 208 208 208 208-93.31 208-208S370.69 48 256 48zm0 319.91a20 20 0 1 1 20-20 20 20 0 0 1-20 20zm21.72-201.15-5.74 122a16 16 0 0 1-32 0l-5.74-121.94v-.05a21.74 21.74 0 1 1 43.44 0z"></path>
                     </svg>
         } else {
-            return <svg />
+            return <svg className="h-6 w-6 flex-none fill-sky-100 stroke-green-500 stroke-2">
+                            <circle cx="12" cy="12" r="11" />
+                            <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+                        </svg>
         }
     }
 
