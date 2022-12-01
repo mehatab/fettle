a361b8d366a13aefedc7e71d9b84eb48b6770b7d
mehatab <mahatab.shaikh@cogoport.com>
2022-12-01 18:37:48 +0530

    log url change

--- src/services/hooks/useServices.tsx
+++ src/services/hooks/useServices.tsx
@@ -46,7 +46,7 @@ function useServices() {
 }
 
 async function logs(key: string): Promise<LogDaySummary[]> {
-    const response = await fetch(`./status/${key}_report.log`);
+    const response = await fetch(`https://raw.githubusercontent.com/mehatab/fettle/main/public/status/${key}_report.log`);
 
     const text = await response.text();
     const lines = text.split("\n");
