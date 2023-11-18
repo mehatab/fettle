3f3f81b169f1e8ded22aef91c5b13afd43ff54cf
Stanimir Stoyanov <stanimir@sellercloud.com>
2023-11-18 17:36:14 +0200

    HCUI typings

---
+++ src/services/types/HealthChecksUI.ts
@@ -0,0 +1,30 @@
+export type HealthChecksUIStatus = "Unhealthy" | "Degraded" | "Healthy";
+
+export interface HealthChecksUILiveness {
+  id: number;
+  name: string;
+  uri: string;
+  onStateFrom: string;
+  lastExecuted: string;
+  status: HealthChecksUIStatus;
+  livenessResult: string;
+  discoveryService: string | null;
+  entries: Array<Check>;
+  history: Array<ExecutionHistory>;
+}
+
+export interface Check {
+  name: string;
+  status: string;
+  description: string | null;
+  duration: string;
+  tags: string[];
+}
+
+export interface ExecutionHistory {
+  name: string;
+  description: string | null;
+  id: number;
+  status: string;
+  on: string;
+}
