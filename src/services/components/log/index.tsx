a32d67b16cf8ff0fd6da18b7fc7cbce8a0210ad4
Stanimir Stoyanov <stanimir@sellercloud.com>
2023-11-19 09:08:55 +0200

    always rounded daily logs

--- src/services/components/log/index.tsx
+++ src/services/components/log/index.tsx
@@ -13,19 +13,19 @@ const ServiceLog: FunctionComponent<ServiceLogProps> = ({ item }) => {
     const statusView = (status: string) => {
         switch (status) {
             case Status.UNKNOWN:
-                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-gray-300 ml-px sm:ml-0.5 sm:rounded-lg flex-1 h-8' >
+                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-gray-300 ml-px sm:ml-0.5 rounded-lg flex-1 h-8' >
                     <StatusView item={item} show={show} />
                 </div>;
             case Status.OUTAGE:
-                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-red-500 ml-px sm:ml-0.5 sm:rounded-lg flex-1 h-8' >
+                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-red-500 ml-px sm:ml-0.5 rounded-lg flex-1 h-8' >
                     <StatusView item={item} show={show} />
                 </div>;
             case Status.PARTIAL_OUTAGE:
-                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-orange-300 ml-px sm:ml-0.5 sm:rounded-lg flex-1 h-8' >
+                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-orange-300 ml-px sm:ml-0.5 rounded-lg flex-1 h-8' >
                     <StatusView item={item} show={show} />
                 </div>;                
             default:
-                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-green-500 ml-px sm:ml-0.5 sm:rounded-lg flex-1 h-8' >
+                return <div onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)} className='bg-green-500 ml-px sm:ml-0.5 rounded-lg flex-1 h-8' >
                     <StatusView item={item} show={show} />
                 </div>;
         }
