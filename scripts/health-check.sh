1d2962a502ef7fa4e0a823f0e1b64715af596528
phyzical <5182053+phyzical@users.noreply.github.com>
2024-02-22 17:19:57 +0800

    inital test

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -1,65 +1,53 @@
 #!/bin/bash
 
-commit=true
+commit=false
 origin=$(git remote get-url origin)
-if [[ $origin == *statsig-io/statuspage* ]]
-then
+if [[ $origin == *statsig-io/statuspage* ]]; then
   commit=false
 fi
 
-declare -a KEYSARRAY
-declare -a URLSARRAY
+wget https://manager.aaf.edu.au/status/monitored_entities.json -O public/entities.json
 
-urlsConfig="public/urls.cfg"
+urlsConfig="public/entities.json"
 echo "Reading $urlsConfig"
-while IFS='=' read -r key url
-do
-  echo "  $key=$url"
-  KEYSARRAY+=("$key")
-  URLSARRAY+=("$url")
-done < "$urlsConfig"
+
+entries=($(jq '.identity_providers[] | "\(.entity_id)~\(.names[] | .value)"' $urlsConfig | grep "http" | sed -r 's/ /_/g'))
 
 echo "***********************"
-echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
-
+rm ../public/urls.cfg
 mkdir -p logs
+for entry in "${entries[@]}"; do
+  echo "$entry"
+  key=$(echo "$entry" | cut -d'~' -f2 | sed -r 's/"//g')
+  url=$(echo "$entry" | cut -d'~' -f1 | sed -r 's/"//g')
+  echo "$key=$url" >>./public/urls.cfg
 
-for (( index=0; index < ${#KEYSARRAY[@]}; index++ ))
-do
-  key="${KEYSARRAY[index]}"
-  url="${URLSARRAY[index]}"
-  echo "  $key=$url"
-
-  for i in {1..3}
-  do
-    response=$(curl -o /dev/null -s -w '%{http_code} %{time_total}' --silent --output /dev/null "$url")
-    http_code=$(echo "$response" | cut -d ' ' -f 1)
-    time_total=$(echo "$response" | cut -d ' ' -f 2)
-    echo "    $http_code $time_total"
-    if [ "$http_code" -eq 200 ] || [ "$http_code" -eq 202 ] || [ "$http_code" -eq 301 ] || [ "$http_code" -eq 302 ] || [ "$http_code" -eq 307 ]; then
-      result="success"
-    else
-      result="failed"
-    fi
-    if [ "$result" = "success" ]; then
-      break
-    fi
-    sleep 5
-  done
-  dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]
-  then
-    mkdir -p public/status
-    echo "$dateTime, $result, $time_total" >> "public/status/${key}_report.log"
-    tail -2000 "public/status/${key}_report.log" > "public/status/${key}_report.log.tmp"
-    mv "public/status/${key}_report.log.tmp" "public/status/${key}_report.log"
+  echo "Starting health check for $key $url"
+  # for i in {1..3}; do
+  # TODO: Do we really want to retry multiple times?
+  response=$(curl -o /dev/null -s -w '%{http_code} %{time_total}' --silent --output /dev/null "$url")
+  http_code=$(echo "$response" | cut -d ' ' -f 1)
+  time_total=$(echo "$response" | cut -d ' ' -f 2)
+  echo "    $http_code $time_total"
+  if [ "$http_code" -eq 200 ] || [ "$http_code" -eq 202 ] || [ "$http_code" -eq 301 ] || [ "$http_code" -eq 302 ] || [ "$http_code" -eq 307 ]; then
+    result="success"
   else
-    echo "    $dateTime, $result, $time_total"
+    result="failed"
   fi
+  # if [ "$result" = "success" ]; then
+  #   break
+  # fi
+  # sleep 5
+  # done
+  dateTime=$(date +'%Y-%m-%d %H:%M')
+  mkdir -p public/status
+  echo "    $dateTime, $result, $time_total"
+  echo "$dateTime, $result, $time_total" >>"public/status/${key}_report.log"
+  tail -2000 "public/status/${key}_report.log" >"public/status/${key}_report.log.tmp"
+  mv "public/status/${key}_report.log.tmp" "public/status/${key}_report.log"
 done
 
-if [[ $commit == true ]]
-then
+if [[ $commit == true ]]; then
   echo "committing logs"
   git config --global user.name 'github-actions[bot]'
   git config --global user.email 'github-actions[bot]@users.noreply.github.com'
--- src/services/hooks/useSystemStatus.tsx
+++ src/services/hooks/useSystemStatus.tsx
@@ -4,85 +4,87 @@ import ServiceStatus from "../types/ServiceStatus";
 import SystemStatus from "../types/SystemStatus";
 
 function useSystemStatus() {
-    const [systemStatus, setSystemStatus] = useState<SystemStatus>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
+  const [systemStatus, setSystemStatus] = useState<SystemStatus>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
-    useEffect(() => {
-        const loadData = async () => {
-            setIsLoading(true);
-            try {
-                const response = await fetch("./urls.cfg");
-                const configText = await response.text();
-                const configLines = configText.split("\n");
-                const services: ServiceStatus[] = [];
-                for (let ii = 0; ii < configLines.length; ii++) {
-                    const configLine = configLines[ii];
-                    const [key, url] = configLine.split("=");
-                    if (!key || !url) {
-                        continue;
-                    }
-                    const status = await logs(key);
+  useEffect(() => {
+    const loadData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("./urls.cfg");
+        const configText = await response.text();
+        const configLines = configText.split("\n");
+        const services: ServiceStatus[] = [];
+        for (let ii = 0; ii < configLines.length; ii++) {
+          const configLine = configLines[ii];
+          const [key, url] = configLine.split("=");
+          if (!key || !url) {
+            continue;
+          }
+          const status = await logs(key);
 
-                    services.push(status);
-                }
-                
-                if (services.every((item) => item.status === "success")) {
-                    setSystemStatus({
-                        title: "All System Operational",
-                        status: Status.OPERATIONAL,
-                        datetime: services[0].date
-                    });
-                } else if (services.every((item) => item.status === "failed")) {
-                    setSystemStatus({
-                        title: "Outage",
-                        status: Status.OUTAGE,
-                        datetime: services[0].date
-                     });
-                } else if (services.every((item) => item.status === "")) {
-                    setSystemStatus({
-                        title: "Unknown",
-                        status: Status.UNKNOWN,
-                        datetime: services[0].date
-                    });
-                } else {
-                    setSystemStatus({
-                        title: "Partial Outage",
-                        status: Status.PARTIAL_OUTAGE,
-                        datetime: services[0].date
-                    });
-                }
-            } catch (e: any) {
-                setError(e);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        loadData();
-    }, []);
+          services.push(status);
+        }
 
-    return {systemStatus, isLoading, error};
+        if (services.every((item) => item.status === "success")) {
+          setSystemStatus({
+            title: "All System Operational",
+            status: Status.OPERATIONAL,
+            datetime: services[0].date,
+          });
+        } else if (services.every((item) => item.status === "failed")) {
+          setSystemStatus({
+            title: "Outage",
+            status: Status.OUTAGE,
+            datetime: services[0].date,
+          });
+        } else if (services.every((item) => item.status === "")) {
+          setSystemStatus({
+            title: "Unknown",
+            status: Status.UNKNOWN,
+            datetime: services[0].date,
+          });
+        } else {
+          setSystemStatus({
+            title: "Partial Outage",
+            status: Status.PARTIAL_OUTAGE,
+            datetime: services[0].date,
+          });
+        }
+      } catch (e: any) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadData();
+  }, []);
+
+  return { systemStatus, isLoading, error };
 }
 
 async function logs(key: string): Promise<ServiceStatus> {
-    const response = await fetch(`https://raw.githubusercontent.com/mehatab/fettle/main/public/status/${key}_report.log`);
-    const text = await response.text();
-    const lines = text.split("\n");
-    try {
-        const line = lines[lines.length - 2];
-        const [created_at, status, _] = line.split(", ");
-        return {
-            name: key,
-            status: status,
-            date: created_at,
-        };
-    } catch (e) {
-        return {
-            name: key,
-            status: "unknown",
-            date: undefined,
-        };
-    }
+  const response = await fetch(
+    `https://raw.githubusercontent.com/mehatab/fettle/main/public/status/${key}_report.log`
+  );
+  const text = await response.text();
+  const lines = text.split("\n");
+  try {
+    const line = lines[lines.length - 2];
+    const [created_at, status, _] = line.split(", ");
+    return {
+      name: key,
+      status: status,
+      date: created_at,
+    };
+  } catch (e) {
+    return {
+      name: key,
+      status: "unknown",
+      date: undefined,
+    };
+  }
 }
 
 export default useSystemStatus;
