4c53d7541edff3b04074fc674e320032e8fe5ffd
Micha Okun <mokun@slac.stanford.edu>
2024-10-18 16:25:16 -0700

    fix logo size; update status every hour

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="w-full h-40 absolute bg-light-purple dark:purple dark:bg-black">
         <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black">
-          <Image src="/s3df-status/slac_s3df_acronym_long_screen_small.png" width={150} height={100} className="w-40 h-16" alt="Tailwind Play" />
+          <Image src="/s3df-status/slac_s3df_acronym_long_screen_small.png" width={150} height={50} className="w-40 h-16" alt="Tailwind Play" />
         </div>
       </div>
       <div className='mt-20 w-full absolute overflow-scroll	'>
