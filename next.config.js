69879c3dace3c8f0cbc0aa7087b662cf8fcf5a49
Micha Okun <mokun@slac.stanford.edu>
2024-10-18 15:08:50 -0700

    try updating script prefix in lieu of custom domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const production = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: production ? '/' : '',
+  assetPrefix: production ? '/s3df-status' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
