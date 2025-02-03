88024485583043f408c160543a2a0efabe0d674e
Murali Shankar <mshankar@slac.stanford.edu>
2025-02-03 12:19:01 -0800

    Remove the extra gray pill to the right

--- src/services/hooks/useServices.tsx
+++ src/services/hooks/useServices.tsx
@@ -103,7 +103,7 @@ function fillData(data: LogDaySummary[]): LogDaySummary[] {
     const logDaySummary: LogDaySummary[] = [];
     var today = new Date();
 
-    for (var i = -1; i < 89; i += 1) {
+    for (var i = 0; i < 89; i += 1) {
         const d = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
         const summary = data.find((item) => item.date === d.toISOString().substr(0, 10));
         logDaySummary.push({
