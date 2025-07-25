00a43ebc5ce5b18538ee4cfe634e0efc1bfde40b
baqueiro-aloi <baqueiro@aloi.tech>
2025-07-25 12:11:31 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div className='h-full w-full '>
       <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="w-full h-40 absolute bg-light-purple dark:purple dark:bg-black">
-        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black">
+        <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-black dark:bg-black">
           <Image src="/aloi.png" alt="Aloi Tech" />
         </div>
       </div>
