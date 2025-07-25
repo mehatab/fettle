1711727e757e93c9cedc43005c7f4dfd4e390cba
baqueiro-aloi <baqueiro@aloi.tech>
2025-07-25 11:15:20 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const production = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: production ? '/' : '',
+  assetPrefix:'',
   reactStrictMode: true,
   swcMinify: true,
 }
